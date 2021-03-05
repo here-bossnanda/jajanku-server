eca5a20aa00b0e39b02f114e9c76be0a75475800
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-03-06 00:50:59 +0700

    [nanda] add api route login and logout

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('login', 'AuthController@login');
+Route::group(['prefix' => 'auth', 'middleware' => 'auth:sanctum'], function() {
+    // manggil controller sesuai bawaan laravel 8
+    Route::post('logout', [AuthController::class, 'logout']);
+    // manggil controller dengan mengubah namespace di RouteServiceProvider.php biar bisa kayak versi2 sebelumnya
+    Route::post('logoutall', 'AuthController@logoutall');
 });
